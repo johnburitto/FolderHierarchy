33221fa73b97747a05e28dd13af31494cd32f0b1
Андрій Борсук <johnburitto@gmail.com>
2023-04-27 15:18:23 +0300

    Add project files.

---
+++ FolderHierarchy/ApiControllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FolderHierarchy.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Hello()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
---
+++ FolderHierarchy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FolderHierarchy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
